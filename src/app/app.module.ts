228f43d92d0e72700fa318f76dead59a1c0960ba
fraagu10 <aguilarfranco1092@protonmail.com>
2023-05-08 15:09:05 -0500

    minor styling updates with fonts and spacing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
